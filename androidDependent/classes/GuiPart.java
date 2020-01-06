9dc989994182c464654b8422f6ba4ccf33951aaa
Sufian <thesfn@users.noreply.github.com>
2020-01-06 11:23:24 +0800

    Update GuiPart.java

--- androidDependent/classes/GuiPart.java
+++ androidDependent/classes/GuiPart.java
@@ -1,11 +1,10 @@
 package xcalibur.androidDependent.classes;
 
 import java.util.List;
+
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -20,6 +19,9 @@ import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import xcalibur.androidDependent.classes.view.HorizontalView;
 import xcalibur.androidDependent.classes.view.ScalableView;
 import xcalibur.androidDependent.interfaces.XADCallback;
